3d2ba20fc2604348c89abbeb2b5de6f302b9cc08
Peter Sammon <petersammon2@gmail.com>
2020-08-14 18:06:26 -0400

    included footer component content

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+
+        <footer class="fixed-bottom">
+            <div class="container">
+                Copyright, Peter Sammon &copy; 2020
+			</div>
+        </footer>
+
+    )
+}
+
+export default Footer
+
