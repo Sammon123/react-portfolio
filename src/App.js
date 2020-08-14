b58b996577e186f62c86bd423668d36c721c9b6b
Peter Sammon <petersammon2@gmail.com>
2020-08-14 18:09:38 -0400

    added navbar component content

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Footer from './components/Footer'
+import Nav from './components/Nav'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <Footer />
     </div>
   );
---
+++ src/components/Nav.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const Nav = () => {
+    return (
+        <div>
+            <header className="navbar navbar-expand-lg navbar-light">
+                <div className="container">
+                    <a className="navbar-brand" href="index.html">Peter Sammon</a>
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-toggle="collapse"
+                        data-target="#navbarNav"
+                        aria-controls="navbarNav"
+                        aria-expanded="True"
+                        aria-label="Toggle navigation"
+                    >
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link" href="index.html">About Me</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="contact.html">Contact</a>
+                            </li>
+                            <li className="nav-item active">
+                                <a className="nav-link" href="portfolio.html">Portfolio</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </header>
+        </div>
+    )
+}
+
+export default Nav
