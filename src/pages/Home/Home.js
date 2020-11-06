b3fd9845704e247820e9b159c6533809e985920f
Peter Sammon <petersammon2@gmail.com>
2020-11-05 20:57:59 -0500

    content for about me page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,30 @@
 import React from 'react'
+import './Home.css';
 
 const Home = () => {
     return (
-        <div>
-            <h1>Home</h1>
-        </div>
+        <main className="container">
+            <div className="container-content">
+                <h1 className="lg-heading">About Me</h1>
+                <img
+                    src="https://lh3.googleusercontent.com/g8j9OYiH7k2uAOZ1aaXZT29-QuUxAZajrkFfqvxTTmIpFmPQTiQDx7TAZokcrMKlPEydc5WpMkUX5imB_wtqE-mkooic6tSYOLWlgOj4fnAI3Q92c1JumKl2PYyOXgU0vgafquQlbdbIgSofwfwCKcSLm3ELQkPRiTwDXCpRPwWg-3jJrOwSQxQGUxVp433XE3AYJRvt2nNk6SvqUdcO-MRmvUdcLOMBHp-Ti26GG5LyOrYJbjIl1OSatCU-FuJ7LBBGkif9iWMfap3drt2cDfH78Fu583F9AuiSF2-D3K5w6edyZJrW3AmYdtWTW-B4FZqMkHss5jtdIYR37TBSolADMOn3OWT0eU7apFyUCzGh2LumuyLoImIud46XoKCHlJbcLdNtzJfc5mCrL2yhe63L2bH0odCR-3cxGsPcFLNkoYpVDASOcU3T0ynSfPidOQgeshq1dDlZu9ktQIBt1cD25L2gd58C9Vz8VyCvwOwkKOF0BnrD0WEVE6L26Zkq2SFtx3lUx4TgK3G3nm_QDnPh7ARi5LkN1ZytPNntpY14dIkbJu4yCRY5QW5nzJb1NHEtomrzW9rO14WSkVtAVGzbsz2MFzbB89NfZ-MVjzh8N4vYpGIEpgvCZgmT6R030IPrsYQ0C4dTGkeDyFNaKWVHnak2IMh8KZLGFuBr1ROh6BvU69WEfEvDTjfe=w596-h794-no?authuser=0"
+                    alt="portrait"
+                    className="portrait"
+                />
+                <p>
+                    Hi I am Peter Sammon, Web developer in training from Dover, NH.
+                    Currently, I am an aspiring developer in training in bootcamp learning
+                    about full stack web development. I thoroughly enjoy building web
+                    applications, anything from basic html to full stack CRUD
+                    applications.
+				</p>
+                <p>
+                    If you are a potential business seeking some sort of web presence
+                    and/or employer looking to hire, feel free to get in touch with me.
+				</p>
+            </div>
+        </main>
+
     )
 }
 
