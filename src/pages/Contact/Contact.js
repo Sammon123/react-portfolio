cacad2311f36d1e68f7fdb3ea4822856622f4b72
Peter Sammon <petersammon2@gmail.com>
2020-11-05 21:16:53 -0500

    added content for contact page

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,10 +1,48 @@
 import React from 'react'
+import './Contact.css'
 
 const Contact = () => {
     return (
-        <div>
-            <h1>Contact</h1>
-        </div>
+        <main id="contact">
+            <h1 className="lg-heading">Contact</h1>
+            <h2 className="sm-heading">Let's get in touch</h2>
+            <h6>Ways to reach me</h6>
+            <ul className="contact">
+                <li>
+                    Email:
+					<a href="https://www.petersammon2@gmail.com">
+                        <span className="text-secondary">
+                            petersammon2@gmail.com
+						</span>
+                    </a>
+                </li>
+                <li>
+                    Phone:
+					<span className="text-secondary">(603) 507-7820</span>
+                </li>
+            </ul>
+
+            <h6 className="sm-heading">Find me on social media</h6>
+            <div className="socials">
+                <ul>
+                    <li>
+                        <a href="https://www.facebook.com/peter.sammon.3" >
+                            <i className="fab fa-facebook"> </i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://github.com/Sammon123"
+                        ><i className="fab fa-github"></i
+                        ></a>
+                    </li>
+                    <li>
+                        <a href="https://www.linkedin.com/in/peter-sammon-950a611a7/"
+                        ><i className="fab fa-linkedin"></i
+                        ></a>
+                    </li>
+                </ul>
+            </div>
+        </main>
     )
 }
 
