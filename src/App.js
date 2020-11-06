6145aa94e94bd39cb6640d1b4b3f788d88384dfe
Peter Sammon <petersammon2@gmail.com>
2020-11-05 20:44:03 -0500

    set up router on the navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar'
 import Footer from './components/Footer/Footer'
+import Home from './pages/Home/Home'
+import Contact from './pages/Contact/Contact'
+import Portfolio from './pages/Portfolio/Portfolio'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/contact">
+            <Contact />
+          </Route>
+          <Route exact path="/portfolio">
+            <Portfolio />
+          </Route>
+        </Switch>
+      </Router>
       <Footer />
     </div>
   );
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import './Navbar.css'
 
 const Navbar = () => {
     return (
         <header className="navbar navbar-expand-lg navbar-light">
             <div className="container">
-                <a className="navbar-brand" href="index.html">Peter Sammon</a>
+                <Link className="navbar-brand" to="/">Peter Sammon</Link>
                 <button
                     className="navbar-toggler"
                     type="button"
@@ -20,13 +21,13 @@ const Navbar = () => {
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
                         <li className="nav-item">
-                            <a className="nav-link" href="index.html">About Me</a>
+                            <Link className="nav-link" to="/">About Me</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="contact.html">Contact</a>
+                            <Link className="nav-link" to="/contact">Contact</Link>
                         </li>
                         <li className="nav-item active">
-                            <a className="nav-link" href="portfolio.html">Portfolio</a>
+                            <Link className="nav-link" to="/portfolio">Portfolio</Link>
                         </li>
                     </ul>
                 </div>
