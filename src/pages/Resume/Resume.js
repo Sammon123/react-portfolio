9b84d6876d77bba8d00457707c0b967089ca266b
Peter Sammon <petersammon2@gmail.com>
2020-12-20 14:32:40 -0500

    added styling to resume page

--- src/pages/Resume/Resume.js
+++ src/pages/Resume/Resume.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import ResumePage from '../../img/Resume.pdf';
+import './Resume.css';
 
 const Resume = () => {
 	return (
-		<div>
-			<h1>Resume</h1>
+		<div className='resume'>
+			<h1>My Resume</h1>
+			<object className='resume-page' data={ResumePage}>
+				Resume
+			</object>
 		</div>
 	);
 };
